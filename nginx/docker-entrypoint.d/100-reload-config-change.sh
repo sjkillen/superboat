03a073a08f641ddb8cad8e76d5af9f92c038bcee
Spencer Killen <sjkillen@ualberta.ca>
2024-06-21 15:36:36 -0600

    .

--- nginx/docker-entrypoint.d/100-reload-config-change.sh
+++ nginx/docker-entrypoint.d/100-reload-config-change.sh
@@ -6,12 +6,16 @@ apt-get install -qy inotify-tools
 watch() {
     while true; do
         inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/routes
+
+        if [ $? -ne 0 ]; then
+            echo "inotify failed";
+            break;
+        fi 
         nginx -t
-        if [ $? -eq 0 ]
-        then
-            echo "Detected Nginx Configuration Change"
-            echo "Executing: nginx -s reload"
-            nginx -s reload
+        if [ $? -eq 0 ]; then
+            echo "Detected Nginx Configuration Change";
+            echo "Executing: nginx -s reload";
+            nginx -s reload;
         fi
     done
 }
